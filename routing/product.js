e3690c84c9e26308488d7d9b9e0923d2c0a47529
pawelwilga <pawel@wilga..net.pl>
2025-04-08 19:52:55 +0200

    Przeróbka add-product na ejs

--- routing/product.js
+++ routing/product.js
@@ -1,4 +1,4 @@
-const fileSystem = require("fs");
+const menu = require('../constants/navigation');
 const { STATUS_CODE } = require("../constants/statusCode");
 
 const path = require("path");
@@ -10,7 +10,7 @@ const renderNewProductPage = require("../views/renderNewProductPage");
 const router = express.Router();
 
 router.get("/add", (_request, response) => {
-  response.sendFile(path.join(__dirname, "../views", "add-product.html"));
+  response.render('add-product', { menuLinks: menu.MENU_LINKS, activeLinkPath: _request.path, headTitle: 'Shop - add product' });
 });
 
 router.post("/add", (request, response) => {
