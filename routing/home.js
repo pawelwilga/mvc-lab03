df39aff1c291286271bda3c78e268a944c39fd7b
pawelwilga <pawel@wilga..net.pl>
2025-04-08 18:04:35 +0200

    Podstawowa zmiana widoku home na ejs

--- routing/home.js
+++ routing/home.js
@@ -4,11 +4,8 @@ const express = require("express");
 
 const router = express.Router();
 
-// 🔄 Refactoro the Changer
-// Funkcja "response.sendFile" nie jest odpowiednia do zwracania widoków generowanych przez
-// większość silników szablonów. Należy ją zastąpić, inną odpowiednią metodą.
 router.get("/", (_request, response) => {
-  response.sendFile(path.join(__dirname, "../views", "home.html"));
+  response.render('home');
 });
 
 module.exports = router;
